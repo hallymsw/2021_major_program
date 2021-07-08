1ad108aac0f4116afdf22f9887c34985ff5c16a4
renige18 <renige18@gmail.com>
2021-07-08 10:59:24 +0900

    Create StudentInfoMain.java

---
+++ 6_java/source/ch06_pr/file/pr01/StudentInfoMain.java
@@ -0,0 +1,11 @@
+package ch06.file.pr01;
+
+public class StudentInfoMain {
+
+	public static void main(String[] args) {
+		String data ="강감찬 2012685 010-1234-5681 경제학과:김유신 2013635 010-1234-5682 수의학과:정약용 2017654 010-1234-5683 통계학과";
+
+
+	}
+
+}
