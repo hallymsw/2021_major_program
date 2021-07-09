d4e848230d3ba160d0f116ebde81e7e4cf116e39
renige18 <renige18@gmail.com>
2021-07-09 10:17:43 +0900

    file pr01 update

--- 6_java/source/ch06_pr/file/pr01/StudentInfoMain.java
+++ 6_java/source/ch06_pr/file/pr01/StudentInfoMain.java
@@ -1,18 +1,89 @@
 package ch06.file.pr01;
 
+import java.util.Scanner;
+
 public class StudentInfoMain {
 
 	public static void main(String[] args) {
-		String data ="강감찬 2012685 010-1234-5681 경제학과:김유신 2013635 010-1234-5682 수의학과:정약용 2017654 010-1234-5683 통계학과";
+		String data ="강감찬 2012685 010-1234-5681 경제학과:김유신 2013635 010-1234-5682 수의학과:김유신 2015635 010-1234-5685 의학과:정약용 2017654 010-1234-5683 통계학과";
+		Scanner sc = new Scanner(System.in);
+		String[] nameArr=null, stNumArr=null, phoneArr=null, deptArr=null;
 		
 		String[] rowArr=data.split(":");
-		System.out.println(rowArr.length);	
-		StudentInfoProcess sip = new StudentInfoProcess(rowArr.length);
+		int[] resultIndex= new int[rowArr.length];
+		int indexCount=0;
+		StudentInfoProcess sip = new StudentInfoProcess(rowArr.length);		
 
 		for(int i=0;i<rowArr.length;i++) {
 			sip.saveDataInArray(rowArr[i]);
 		}
-		sip.printArray();
+		
+		nameArr=sip.getNameArr();
+		stNumArr=sip.getStNumArr();
+		phoneArr=sip.getPhoneArr();
+		deptArr=sip.getDeptArr();
+
+		while(true) {
+			System.out.print("select a number(1-4) : ");
+			int selNum = sc.nextInt();
+
+			if(selNum==1) {
+				System.out.print("input name : ");
+				String name= sc.next();
+				resultIndex=sip.getIndexArrByName(name);
+				
+				for(int i=0;i<resultIndex.length;i++) {
+					if(resultIndex[i]==1){
+						System.out.println(stNumArr[i]+" "+phoneArr[i]+" "+deptArr[i]);
+					}
+					
+				}
+				
+			}else if(selNum==2) {
+
+				System.out.print("input student number : ");
+				String number= sc.next();
+				resultIndex=sip.getIndexArrByNumber(number);
+				
+				for(int i=0;i<resultIndex.length;i++) {
+					if(resultIndex[i]==1){
+						System.out.println(nameArr[i]+" "+phoneArr[i]);
+					}
+					
+				}
+				
+			}else if(selNum==3) {
+				System.out.print("input deptment : ");
+				String number= sc.next();
+				resultIndex=sip.getIndexArrByDept(number);
+				
+				for(int i=0;i<resultIndex.length;i++) {
+					if(resultIndex[i]==1){
+						System.out.println(nameArr[i]+" "+stNumArr[i]);
+					}
+					
+				}
+				
+			}else if(selNum==4) {
+				System.out.print("input phone number : ");
+				String phone= sc.next();
+				resultIndex=sip.getIndexArrByPhone(phone);
+				
+				for(int i=0;i<resultIndex.length;i++) {
+					if(resultIndex[i]==1){
+						System.out.println(nameArr[i]+" "+stNumArr[i]+" "+deptArr[i]);
+					}
+					
+				}
+				
+			}else {
+				System.out.println("error");
+			}
+			
+			
+		}
+		
+		
 		
 
 	}
--- 6_java/source/ch06_pr/file/pr01/StudentInfoProcess.java
+++ 6_java/source/ch06_pr/file/pr01/StudentInfoProcess.java
@@ -6,6 +6,20 @@ public class StudentInfoProcess {
 	String[] phoneArr;
 	String[] deptArr;
 	int indexCount=0;
+	
+	public String[] getNameArr() {
+		return nameArr;
+	}
+	public String[] getStNumArr() {
+		return stNumArr;
+	}
+	public String[] getPhoneArr() {
+		return phoneArr;
+	}
+	public String[] getDeptArr() {
+		return deptArr;
+	}
+	
 	public StudentInfoProcess(int len) {
 		nameArr = new String[len];
 		stNumArr = new String[len];
@@ -22,15 +36,45 @@ public class StudentInfoProcess {
 		indexCount++;
 		return result;
 	}
-	public void printArray() {
-		for(int i=0;i<indexCount;i++) {
-			System.out.println(nameArr[i]);	
-			System.out.println(stNumArr[i]);	
-			System.out.println(phoneArr[i]);	
-			System.out.println(deptArr[i]);	
-		}
-	}
-	
-	
+	//인덱스 값에 0도 들어 있을 수 있으므로 반드시 인덱스를 가진 배열 길이도 알아야 한다.
+	public int[] getIndexArrByName(String name) {
+		int[] idxArr=new int[nameArr.length];
 
+		for(int i=0;i<nameArr.length;i++) {
+			if(name.equals(nameArr[i])) {
+				idxArr[i]=1;			
+			}
+		}
+		return idxArr;
+	}
+	public int[] getIndexArrByNumber(String num) {
+		int[] idxArr=new int[stNumArr.length];
+
+		for(int i=0;i<stNumArr.length;i++) {
+			if(num.equals(stNumArr[i])) {
+				idxArr[i]=1;			
+			}
+		}
+		return idxArr;
+	}
+	public int[] getIndexArrByDept(String dept) {
+		int[] idxArr=new int[deptArr.length];
+
+		for(int i=0;i<deptArr.length;i++) {
+			if(dept.equals(deptArr[i])) {
+				idxArr[i]=1;			
+			}
+		}
+		return idxArr;
+	}
+	public int[] getIndexArrByPhone(String phone) {
+		int[] idxArr=new int[phoneArr.length];
+
+		for(int i=0;i<deptArr.length;i++) {
+			if(phone.equals(phoneArr[i])) {
+				idxArr[i]=1;			
+			}
+		}
+		return idxArr;
+	}
 }
